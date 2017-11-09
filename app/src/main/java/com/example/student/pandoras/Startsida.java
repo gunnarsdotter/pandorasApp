fb112afc3133a154ddd5bd04616c9a7cf7e9cc85
Petra <gunnarsdotter@live.com>
2017-11-09 11:12:30 +0100

    ja den andra committen funkade inte så app ichh idea ändrad inte gradlen för de är lokal för datorn

--- app/src/main/java/com/example/student/pandoras/Startsida.java
+++ app/src/main/java/com/example/student/pandoras/Startsida.java
@@ -2,6 +2,7 @@ package com.example.student.pandoras;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Startsida extends AppCompatActivity {
 
@@ -10,4 +11,15 @@ public class Startsida extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startsida);
     }
+
+    public void openStartsida(View v){
+        setContentView(R.layout.startsida);
+    }
+    public void openAddQuestion(View v){
+        setContentView(R.layout.add_questionpage);
+    }
+    public void openQuestion(View v){
+        setContentView(R.layout.questionpage);
+    }
+
 }
