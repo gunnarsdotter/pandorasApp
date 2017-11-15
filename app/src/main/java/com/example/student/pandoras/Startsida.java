e9ea314a74194fea4e1925ce2ca41d7959c990e7
Petra <gunnarsdotter@live.com>
2017-11-15 21:52:04 +0100

    class question-skapar frågor class qude- sparar frågorna i en arraylist

--- app/src/main/java/com/example/student/pandoras/Startsida.java
+++ app/src/main/java/com/example/student/pandoras/Startsida.java
@@ -3,11 +3,15 @@ package com.example.student.pandoras;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Startsida extends AppCompatActivity {
-
+    public Qude myQude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        myQude = new Qude();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startsida);
     }
@@ -20,8 +24,27 @@ public class Startsida extends AppCompatActivity {
     }
     public void openQuestion(View v){
         setContentView(R.layout.questionpage);
+        TextView textViewer = findViewById(R.id.textView3);
+        textViewer.setText(myQude.getQuestion().concat(Integer.toString(myQude.getRandomNumber())));
     }
     public void openInfo(View v) {
         v.setBackgroundResource(R.color.colorPrimaryDark);
     }
+
+    public void add_question(View v){
+        EditText textWriter = findViewById(R.id.editText);
+        if(textWriter.getText().toString() != ""){
+            myQude.addQuestion(new Question(textWriter.getText().toString()));
+            textWriter.setText("Du har lagt till en fråga");
+        }
+
+    }
+    public void next_question(View v) {
+        myQude.removeQuestion();
+        TextView textViewer = findViewById(R.id.textView3);
+        textViewer.setText(myQude.getQuestion().concat(Integer.toString(myQude.getRandomNumber())));
+        Button buttonChangeText = findViewById(R.id.button4);
+        buttonChangeText.setText(getResources().getString(R.string.new_game));
+    }
+
 }
